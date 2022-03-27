8a897efc8abc7b99684de5e3726ca7b535a75eff
RyosukeSomeya <rx.0.0618@gmail.com>
2022-03-28 00:54:27 +0900

    管理画面設定（Model紐付け）

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Segment, Brand, Vehicle
 # Register your models here.
+
+# 管理画面とModelの紐付け
+admin.site.register(Segment)
+admin.site.register(Brand)
+admin.site.register(Vehicle)
