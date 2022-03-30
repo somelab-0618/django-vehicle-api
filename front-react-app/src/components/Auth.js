3cdec74ebf97697172c8da1e9aa99768d9555a0e
RyosukeSomeya <rx.0.0618@gmail.com>
2022-03-30 23:45:35 +0900

    react app ベース作成

---
+++ front-react-app/src/components/Auth.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const auth = () => {
+  return <div>Welcome to Auth</div>;
+};
+
+export default auth;
---
+++ front-react-app/src/components/Segment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Segment = () => {
+  return <div>Segment</div>;
+};
+
+export default Segment;
